c410a64e513480e77d87d5d50678cf41d78d1fff
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2014-04-27 21:14:04 +0100

    NoTask - cleaned up formatting in routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 var app = module.parent.exports.app;
 
 // controllers
-var webController   = require('./controllers/web');
+var webController = require('./controllers/web');
 
 // web requests
 app.get('/', webController.index);

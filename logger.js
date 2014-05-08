6f9afd48ed808dcf94e1ceb833ec7b25fdd24b8a
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2014-05-08 21:00:38 +0100

    NoTask - updated logger and removed bower from package.json

--- logger.js
+++ logger.js
@@ -3,25 +3,16 @@ var winston = require('winston');
 var logger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({
-      timestamp: true,
       colorize: true
     }),
     new (winston.transports.File)({
       filename: __dirname + '/logs/main.log',
-      json: true,
-      colorize: false,
-      maxsize: 10485760
+      maxsize: 10485760,
     })
   ],
   exceptionHandlers: [
-    new (winston.transports.Console)({
-      timestamp: true,
-      colorize: true
-    }),
     new (winston.transports.File)({
       filename: __dirname + '/logs/errors.log',
-      json: true,
-      colorize: false,
       maxsize: 10485760
     })
   ],
